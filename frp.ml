a19b24087aaeea82a47fdec74b9a1278333be9d4
Izaak Meckler <ihmeckler@gmail.com>
2014-01-07 11:04:36 -0500

    Initial commit

---
+++ frp.ml
@@ -0,0 +1,82 @@
+module Stream = struct
+  type 'a t =
+    { mutable listeners : ('a -> unit) list
+    ; mutable value     : 'a
+(*     ; mutable uid       : int *)
+    }
+
+  let set t x = t.value <- x
+
+  let add_listener t f =
+    t.listeners <- f :: t.listeners 
+
+  let trigger t x =
+    t.value <- x;
+    List.iter t.listeners ~f:(fun f -> f x)
+
+  let iter t ~f = add_listener t f
+
+  let create init = { value = init ; listeners = [] }
+
+  let map t ~f =
+    let t' = create (f t.value) in
+    add_listener t (fun x -> trigger t' (f x));
+    t'
+  ;;
+
+  let filter t ~f =
+    let t' = create t.value in
+    add_listener t (fun x -> if f x then trigger t' x);
+  ;;
+
+  let fold t ~init ~f =
+    let t' = create init in
+    add_listener t (fun x -> trigger t' (f t'.value x));
+    t'
+  ;;
+
+  let zip_with t1 t2 ~f =
+    let t' = create (f t1.value t2.value) in
+    add_listener t1 (fun x -> trigger t' (f x t2.value));
+    add_listener t2 (fun y -> trigger t' (f t1.value y));
+    t'
+  ;;
+
+  let zip t1 t2 = zip_with ~f:(fun x y -> (x, y))
+
+  let merge t1 t2 =
+    let t' = create t1.value in
+    add_listener t1 (fun x -> trigger t' x);
+    add_listener t2 (fun x -> trigger t' x);
+    t'
+  ;;
+
+  let when_ cond t =
+    let t' = create t.value in
+    add_listener t (fun x -> if cond.value then trigger t' x;)
+  ;;
+
+  let join t =
+    let t' = create t.value.value in
+    add_listener t.value (fun x -> trigger t' x);
+    add_listener t (fun s ->
+      add_listener s (fun x -> trigger t' x)
+    );
+    t'
+  ;;
+
+  let return = create
+
+  let bind t ~f = join (map t ~f)
+
+  module Infix = struct
+    let (>>=) t f = bind t ~f
+
+    let (>>|) t f = map t ~f
+
+    let (<$>) f t = map x ~f
+    
+    let (<*>) tf tx = zip_with tf tx ~f:(fun f x -> f x)
+  end
+end
+
---
+++ frp.mli
@@ -0,0 +1,34 @@
+module Stream : sig
+  type 'a t
+
+  val create : 'a -> 'a t
+
+  val trigger : 'a t -> 'a -> unit
+
+  val map : 'a t -> f:('a -> 'b) -> 'b t
+
+  val iter : 'a t -> f:('a -> unit) -> unit
+
+  val filter : 'a t -> f:('a -> bool) -> 'a t
+
+  val fold : 'a t -> init:'accum -> f:('accum -> 'a -> 'accum) -> 'accum t
+
+  val zip_with : 'a t -> 'b t -> f:('a -> 'b -> 'c) -> 'c t
+
+  val merge : 'a t -> 'a t -> 'a t
+
+  val when_ : bool t -> 'a t -> 'a t
+
+  val join : 'a t t -> 'a t
+
+  val return : 'a -> 'a t
+
+  val bind : 'a t -> f:('a -> 'b t) -> 'b t
+
+  module Infix : sig
+    val (>>=) : 'a t -> ('a -> 'b t) -> 'b t
+
+    val (>>|) : 'a t -> ('a -> 'b) -> 'b t
+  end
+end
+
